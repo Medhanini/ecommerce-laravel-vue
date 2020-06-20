4f263cd94a8385af2900fd4e10480cc280aa38e9
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-20 17:13:39 +0100

    Updating model Product

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+    use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+
+        protected $fillable = [
+            'name', 'price', 'units', 'description', 'image'
+        ];
+
+        public function orders(){
+            return $this->hasMany(Order::class);
+        }
 }
