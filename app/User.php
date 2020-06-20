2d5d7ac72f61e2ac5e18de4d5f7b27fe3fcdc802
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-20 17:12:24 +0100

    Updating model User

--- app/User.php
+++ app/User.php
@@ -5,10 +5,15 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+    use Illuminate\Database\Eloquent\SoftDeletes;
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes; 
+
+        public function orders()
+        {
+            return $this->hasMany(Order::class);
+        }
 
     /**
      * The attributes that are mass assignable.
