888952a83bf9a8131dabba7839006ac9a1606cfd
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-20 17:22:51 +0100

    Creating seeder: UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = "Admin";
+        $user->email = "admin@devtest.com";
+        $user->password = bcrypt('secret');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
