338f36ad0fbc69bc156ea7f5873522f1faa7676b
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-20 17:16:13 +0100

    Defining the migration products of the app

--- database/migrations/2020_06_20_160834_create_products_table.php
+++ database/migrations/2020_06_20_160834_create_products_table.php
@@ -14,8 +14,14 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->unsignedInteger('units')->default(0);
+            $table->double('price');
+            $table->string('image');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
