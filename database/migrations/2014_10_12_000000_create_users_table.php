fe02b911c44ffb9fdb0941a2349c3a9920e1fbf5
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-20 17:33:43 +0100

    fixing users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->increments('id');
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->boolean('is_admin')->default(false);
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+                $table->softDeletes();
         });
     }
 
