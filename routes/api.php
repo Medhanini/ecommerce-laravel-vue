6569981c22df54a3de2e41b7dc8d2102e16bfb01
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-22 13:17:23 +0100

    Defining our application’s routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::get('/products', 'ProductController@index');
+Route::post('/upload-file', 'ProductController@uploadFile');
+Route::get('/products/{product}', 'ProductController@show');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('/users','UserController@index');
+    Route::get('users/{user}','UserController@show');
+    Route::patch('users/{user}','UserController@update');
+    Route::get('users/{user}/orders','UserController@showOrders');
+    Route::patch('products/{product}/units/add','ProductController@updateUnits');
+    Route::patch('orders/{order}/deliver','OrderController@deliverOrder');
+    Route::resource('/orders', 'OrderController');
+    Route::resource('/products', 'ProductController')->except(['index','show']);
 });
