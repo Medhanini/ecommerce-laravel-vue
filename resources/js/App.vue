6b29e299d235ddda798c6ee773cfe46308c59594
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-22 13:29:03 +0100

    creation App.vue application container where all other components will be loaded

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+        <div>
+            <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+                <div class="container">
+                    <router-link :to="{name: 'home'}" class="navbar-brand">Big Store</router-link>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto"></ul>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <router-link :to="{ name: 'login' }" class="nav-link" v-if="!isLoggedIn">Login</router-link>
+                            <router-link :to="{ name: 'register' }" class="nav-link" v-if="!isLoggedIn">Register</router-link>
+                            <span v-if="isLoggedIn">
+                                <router-link :to="{ name: 'userboard' }" class="nav-link" v-if="user_type == 0"> Hi, {{name}}</router-link>
+                                <router-link :to="{ name: 'admin' }" class="nav-link" v-if="user_type == 1"> Hi, {{name}}</router-link>
+                            </span>
+                            <li class="nav-link" v-if="isLoggedIn" @click="logout"> Logout</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <main class="py-4">
+                <router-view @loggedIn="change"></router-view>
+            </main>
+        </div>
+    </template>
