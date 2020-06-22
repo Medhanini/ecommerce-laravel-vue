1ea23fc11760e7903947cc26ff84c69d423d3c63
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-22 13:13:16 +0100

    Building ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Product::all(),200);
     }
 
     /**
@@ -35,7 +35,19 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'units' => $request->units,
+            'price' => $request->price,
+            'image' => $request->image
+        ]);
+
+        return response()->json([
+            'status' => (bool) $product,
+            'data'   => $product,
+            'message' => $product ? 'Product Created!' : 'Error Creating Product'
+        ]);
     }
 
     /**
@@ -46,7 +58,15 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return response()->json($product,200); 
+    }
+    public function uploadFile(Request $request)
+    {
+        if($request->hasFile('image')){
+            $name = time()."_".$request->file('image')->getClientOriginalName();
+            $request->file('image')->move(public_path('images'), $name);
+        }
+        return response()->json(asset("images/$name"),201);
     }
 
     /**
@@ -69,8 +89,25 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $status = $product->update(
+            $request->only(['name', 'description', 'units', 'price', 'image'])
+        );
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Product Updated!' : 'Error Updating Product'
+        ]);
     }
+    public function updateUnits(Request $request, Product $product)
+        {
+            $product->units = $product->units + $request->get('units');
+            $status = $product->save();
+
+            return response()->json([
+                'status' => $status,
+                'message' => $status ? 'Units Added!' : 'Error Adding Product Units'
+            ]);
+        }
 
     /**
      * Remove the specified resource from storage.
@@ -79,7 +116,12 @@ class ProductController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
-    {
-        //
-    }
+        {
+            $status = $product->delete();
+
+            return response()->json([
+                'status' => $status,
+                'message' => $status ? 'Product Deleted!' : 'Error Deleting Product'
+            ]);
+        }
 }
