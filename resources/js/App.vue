1457fad734feecdc5a9a65e9d09cf50f60518b7d
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-22 13:29:58 +0100

    creation App.vue application container where all other components will be loaded

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -27,3 +27,36 @@
             </main>
         </div>
     </template>
+<script>
+    export default {
+        data() {
+            return {
+                name: null,
+                user_type: 0,
+                isLoggedIn: localStorage.getItem('bigStore.jwt') != null
+            }
+        },
+        mounted() {
+            this.setDefaults()
+        },
+        methods : {
+            setDefaults() {
+                if (this.isLoggedIn) {
+                    let user = JSON.parse(localStorage.getItem('bigStore.user'))
+                    this.name = user.name
+                    this.user_type = user.is_admin
+                }
+            },
+            change() {
+                this.isLoggedIn = localStorage.getItem('bigStore.jwt') != null
+                this.setDefaults()
+            },
+            logout(){
+                localStorage.removeItem('bigStore.jwt')
+                localStorage.removeItem('bigStore.user')
+                this.change()
+                this.$router.push('/')
+            }
+        }
+    }
+    </script>
