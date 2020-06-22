fbc9d72cf8ae57e1770be3a266a5a28d8df5056a
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-22 13:15:21 +0100

    Building OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Order;
+use Auth;
 use Illuminate\Http\Request;
-
 class OrderController extends Controller
 {
     /**
@@ -14,9 +14,19 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Order::with(['product'])->get(),200);
     }
+    public function deliverOrder(Order $order)
+    {
+        $order->is_delivered = true;
+        $status = $order->save();
 
+        return response()->json([
+            'status'    => $status,
+            'data'      => $order,
+            'message'   => $status ? 'Order Delivered!' : 'Error Delivering Order'
+        ]);
+    }
     /**
      * Show the form for creating a new resource.
      *
@@ -35,7 +45,18 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $order = Order::create([
+            'product_id' => $request->product_id,
+            'user_id' => Auth::id(),
+            'quantity' => $request->quantity,
+            'address' => $request->address
+        ]);
+
+        return response()->json([
+            'status' => (bool) $order,
+            'data'   => $order,
+            'message' => $order ? 'Order Created!' : 'Error Creating Order'
+        ]);
     }
 
     /**
@@ -45,9 +66,9 @@ class OrderController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Order $order)
-    {
-        //
-    }
+        {
+            return response()->json($order,200);
+        }
 
     /**
      * Show the form for editing the specified resource.
@@ -68,9 +89,16 @@ class OrderController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Order $order)
-    {
-        //
-    }
+        {
+            $status = $order->update(
+                $request->only(['quantity'])
+            );
+
+            return response()->json([
+                'status' => $status,
+                'message' => $status ? 'Order Updated!' : 'Error Updating Order'
+            ]);
+        }
 
     /**
      * Remove the specified resource from storage.
@@ -79,7 +107,12 @@ class OrderController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(Order $order)
-    {
-        //
-    }
+        {
+            $status = $order->delete();
+
+            return response()->json([
+                'status' => $status,
+                'message' => $status ? 'Order Deleted!' : 'Error Deleting Order'
+            ]);
+        }
 }
